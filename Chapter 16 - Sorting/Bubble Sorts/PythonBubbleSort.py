7f3cb267ea7bd72ed8316d0db76b209981b4b06c
Andrei Patea <51858877+Thrill12@users.noreply.github.com>
2022-03-28 23:40:18 +0100

    Comments woo

--- Chapter 16 - Sorting/Bubble Sorts/PythonBubbleSort.py
+++ Chapter 16 - Sorting/Bubble Sorts/PythonBubbleSort.py
@@ -4,7 +4,9 @@ def bubble_sort(our_list):
         # We want the last pair of adjacent elements to be (n-2, n-1)
         for j in range(len(our_list) - 1):
             if our_list[j] > our_list[j+1]:
-                # Swap
+                # Swap - python doesn't necessarily require a temporary variable, however that would work just as well
+                # This is a python only feature I'm afraid, as their internal lists are a bit strange and work in
+                # a unique way
                 our_list[j], our_list[j+1] = our_list[j+1], our_list[j]
 
 ourList = [5, 4, 2, 8, 9, 15, 2, 6, 5, 12]
