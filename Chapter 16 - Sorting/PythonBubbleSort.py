5553e724ac4a8949b5dce3ba253632a564fbc23d
Andrei Patea <51858877+Thrill12@users.noreply.github.com>
2022-03-28 21:43:01 +0100

    Added to python code

--- Chapter 16 - Sorting/PythonBubbleSort.py
+++ Chapter 16 - Sorting/PythonBubbleSort.py
@@ -6,3 +6,7 @@ def bubble_sort(our_list):
             if our_list[j] > our_list[j+1]:
                 # Swap
                 our_list[j], our_list[j+1] = our_list[j+1], our_list[j]
+
+ourList = [5, 4, 2, 8, 9, 15, 2, 6, 5, 12]
+bubble_sort(ourList)
+print(ourList)
