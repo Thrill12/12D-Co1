1d52aef61476324bf61560474e5d15743400e450
Andrei Patea <51858877+Thrill12@users.noreply.github.com>
2022-03-29 09:10:56 +0100

    Added to C# code

--- Chapter 16 - Sorting/Bubble Sorts/C#BubbleSort.cs
+++ Chapter 16 - Sorting/Bubble Sorts/C#BubbleSort.cs
@@ -4,11 +4,11 @@ int temp = 0;
 
 for (int write = 0; write < arr.Length; write++)            //Looping until the end of the array
 {
-    for (int sort = 0; sort < arr.Length - 1; sort++)       //Looping until 
+    for (int sort = 0; sort < arr.Length - 1; sort++)       
     {
-        if (arr[sort] > arr[sort + 1]) 
+        if (arr[sort] > arr[sort + 1])                      //Compares the two numbers
         {
-            temp = arr[sort + 1];
+            temp = arr[sort + 1];                           //Performs the swap
             arr[sort + 1] = arr[sort];
             arr[sort] = temp;
         }
